79380de4154a09e4901ac5078630964111412ac6
Jonathan Bechtel <jonathanbechtel@gmail.com>
2020-09-27 18:09:33 -0400

    uploaded seaborn charts

--- app.py
+++ app.py
@@ -8,59 +8,31 @@ Created on Sun Sep 27 15:53:06 2020
 import streamlit as st
 import pandas as pd
 import pickle
+import seaborn as sns
 
 # list of columns to use for app
-cols = ['Id',
- 'MSSubClass',
- 'MSZoning',
- 'LotArea',
- 'Neighborhood',
- 'OverallQual',
- 'OverallCond',
- 'YearBuilt',
- 'GrLivArea',
- '1stFlrSF',
- '2ndFlrSF',
- 'GrLivArea.1',
- 'FullBath',
- 'HalfBath',
- 'GarageType',
- 'GarageYrBlt',
- 'GarageFinish',
- 'GarageCars',
- 'SalePrice']
+cols = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual', 'OverallCond', 'YearBuilt', 'FullBath', 'HalfBath', 'GarageType']
 
-num_cols = ['Id',
- 'MSSubClass',
- 'LotArea',
- 'OverallQual',
- 'OverallCond',
- 'YearBuilt',
- 'GrLivArea',
- '1stFlrSF',
- '2ndFlrSF',
- 'GrLivArea.1',
- 'FullBath',
- 'HalfBath',
- 'GarageYrBlt',
- 'GarageCars',
- 'SalePrice']
-
+num_cols = ['OverallQual', 'OverallCond', 'GrLivArea', 'SalePrice', 'LotArea', 'SalePrice']
 # function for loading in the grouping file -- notice the use of @st.cache -- speeds things up
 
 st.title('Iowa Housing Data Application')
 
+# loads in the grouping file to be used for bar/line charts
 @st.cache
 def load_grouping_file(x_col, y_col):
     filepath = f"https://raw.githubusercontent.com/JonathanBechtel/optimized-app/master/groupbys/{x_col}_{y_col}.csv"
     print(filepath)
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, index_col=x_col)
     return df
 
+# loads in the seaborn boxplot
 @st.cache
-def load_boxplot_dict():
-    with open('boxplots.pkl', 'rb') as boxplot_dict:
-        boxplots = pickle.load(boxplot_dict)
+def load_boxplots(x_col, y_col):
+    pickled_boxplot = f"boxplots/{x_col}_{y_col}.pkl"
+    with open(pickled_boxplot, 'rb') as boxplot_dict:
+        chart = pickle.load(boxplot_dict)
+    return chart
         
 @st.cache
 def load_seaborn_boxplot(boxplot_dict, x_axis, y_axis):
@@ -74,8 +46,8 @@ if section == 'Data Explorer':
     
     st.subheader("Build Charts From Sidebar Choices")
     
-    x_axis = st.sidebar.selectbox('Choose Column For X-Axis', cols, index=1)
-    y_axis = st.sidebar.selectbox('Choose Column For Y-Axis', num_cols, index=14)
+    x_axis = st.sidebar.selectbox('Choose Column For X-Axis', cols)
+    y_axis = st.sidebar.selectbox('Choose Column For Y-Axis', num_cols)
     
     chart_type = st.sidebar.selectbox('Choose Chart Type', ['bar', 'line', 'box'])
     
@@ -94,9 +66,8 @@ if section == 'Data Explorer':
             st.text("Could not find data for specified column combinations")
             
     elif chart_type == 'box':
-        boxplots = load_boxplot_dict()
         try:
-            chart = load_seaborn_boxplot(boxplots, x_axis, y_axis)
-            st.pyplot(chart.figure)
+            chart = load_seaborn_boxplot(x_axis, y_axis)
+            st.pyplot(chart)
         except:
             st.text("Could not find seaborn chart figure for specified columns")
